95a0029450f4dca48c363e6eae428ed5f5980063
Le Dai Nguyen <128657481+DnMakyr@users.noreply.github.com>
2025-01-15 02:38:33 +0700

    Delete WinnerView.vue

--- src/views/WinnerView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="pt-36 flex flex-col justify-center items-center align-middle space-y-4">
-
-    <h1>Winner View</h1>
-    <p>This is the winner view</p>
-  </div>
-</template>
