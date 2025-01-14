bbafda84a2d3db5cd37d7304a55aaae2b55cb44d
Le Dai Nguyen <128657481+DnMakyr@users.noreply.github.com>
2025-01-15 02:38:50 +0700

    chore: major changes in UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAttendantsStore } from '@/stores/attendants'
-import { columns } from '@/components/employee/columns'
-import DataTable from '@/components/DataTable.vue'
 import { Button } from '@/components/ui/button'
 import type { Attendant } from '@/types/attendant'
 import { useFetchAttendants } from '@/composables/useFetchAttendants'
 import OpaqueBox from '@/components/OpaqueBox.vue'
 
 const router = useRouter()
-const showTable = ref(false)
-
-const toggleTable = () => {
-  showTable.value = !showTable.value
-}
-
-const buttonLabel = computed(() => (showTable.value ? 'Ẩn' : 'Hiện') + " Danh Sách Người Tham Gia")
 
 const attendants = ref<Attendant[]>([])
 
@@ -42,14 +33,13 @@ onMounted(async () => {
     </OpaqueBox>
     <div class="flex flex-col justify-center items-center mt-10">
       <div class="space-x-4 flex justify-center items-center">
-        <Button @click="router.push('/consolation')" class="red-spring">Bốc Giải Khuyến Khích</Button>
-        <Button @click="router.push('/third')" class="red-spring">Bốc Giải Ba</Button>
-        <Button @click="router.push('/second')" class="red-spring">Bốc Giải Nhì</Button>
-        <Button @click="router.push('/first')" class="red-spring">Bốc Giải Nhất</Button>
-        <Button @click="router.push('/deluxe')" class="red-spring">Bốc Giải Đặc Biệt</Button>
+        <Button @click="router.push('/consolation')" class="red-spring">Giải Khuyến Khích</Button>
+        <Button @click="router.push('/third')" class="red-spring">Giải Ba</Button>
+        <Button @click="router.push('/second')" class="red-spring">Giải Nhì</Button>
+        <Button @click="router.push('/first')" class="red-spring">Giải Nhất</Button>
+        <Button @click="router.push('/deluxe')" class="red-spring">Giải Đặc Biệt</Button>
+        <Button @click="router.push('/desk')" class="red-spring">Bàn Làm Việc May Mắn</Button>
       </div>
-      <Button class="my-5" @click="toggleTable">{{ buttonLabel }}</Button>
-      <DataTable v-show="showTable" :columns="columns" :data="attendants" />
     </div>
   </div>
 </template>
--- src/views/ThirdView.vue
+++ src/views/ThirdView.vue
@@ -1,46 +1,113 @@
 <script setup lang="ts">
 import { useLotteryDrawing } from '@/composables/useLotteryDrawing'
 import { Button } from '@/components/ui/button'
-import { ref, onMounted, unref, computed } from 'vue'
+import { ref, computed, useTemplateRef, nextTick, watch } from 'vue'
 import { useFetchWinners } from '@/composables/useFetchWinners'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
+import OpaqueBox from '@/components/OpaqueBox.vue'
+import TextAnimation from '@/components/TextAnimation.vue'
+import music from '@/assets/audio/Nhac-Chuong-xo-so-mien-bac.mp3'
 
-const { pendingPrize, prizeWinners, fetchWinners, fetchPrizeWinners } = useFetchWinners()
+const { pendingPrize, fetchWinners } = useFetchWinners()
 
-const { currentWinners, thirdPrizeDrawing } = useLotteryDrawing()
-const drawCount = ref(3)
-const drawing = async () => {
-  thirdPrizeDrawing()
-  drawCount.value--
-  fetchWinners()
-}
+const { currentWinners, winners: { thirdWinners }, thirdPrizeDrawing } = useLotteryDrawing()
+
+const drawCount = ref(2)
 
 const drawable = computed(() => {
-  if (unref(prizeWinners).length >= 15) return false
-  if (unref(drawCount) <= 0) return false
+  // if (unref(prizeWinners).length >= 2) return false
+  // if (unref(drawCount) <= 0) return false
   return true
 })
-onMounted(async () => {
-  fetchPrizeWinners('third')
-})
+
+const isDrawing = ref(false)
+
+const winnerList = useTemplateRef('winner-list')
+
+const scrollToBottom = async () => {
+  await nextTick();
+  if (winnerList.value) {
+    winnerList.value.scrollTop = winnerList.value.scrollHeight;
+  }
+};
+
+const audio = new Audio(music);
+const drawing = async () => {
+
+  isDrawing.value = true;
+  audio.play();
+  thirdPrizeDrawing();
+  drawCount.value--;
+  fetchWinners();
+
+  setTimeout(async () => {
+    isDrawing.value = false;
+    audio.pause();
+    await scrollToBottom();
+  }, 15000);
+};
+
+watch(thirdWinners, scrollToBottom);
+
 </script>
 
 <template>
-  <LoadingSpinner v-if="pendingPrize" />
-  <div v-else class="pt-36 flex flex-col justify-center items-center align-middle space-y-4">
-    <p class="font-mono font-semibold text-2xl">Bốc Giải Ba</p>
-    <Button @click="drawing" :disabled="!drawable">Bốc Giải</Button>
-    <div v-if="currentWinners && currentWinners.length > 0">
-      <p class="font-mono font-semibold text-2xl text-center">Người Thắng Giải</p>
-      <div class="flex flex-col items-center space-y-4">
-        <div v-for="winner in currentWinners" :key="winner.id"
-          class="flex justify-center items-center space-x-4 text-xl"
-          :class="winner.type === 'factory' ? 'text-blue-400' : 'text-slate-500'">
-          <p class="font-sans font-semibold">{{ winner.code }}</p>
-          <span>-</span>
-          <p class="font-sans font-semibold">{{ winner.name }}</p>
+  <div class="min-h-screen flex flex-col items-center justify-between px-4 pt-14 pb-4">
+    <div class="flex flex-col items-center space-y-6">
+      <LoadingSpinner v-if="pendingPrize" />
+      <div v-else class="flex flex-col items-center">
+        <div class="p-4 rounded-lg w-fit" style="background-color: rgba(255, 255, 255, 0.7);">
+          <p class="font-mono font-semibold uppercase text-4xl text-center text-red-800">Giải Ba</p>
         </div>
+        <TextAnimation v-show="isDrawing" />
+        <transition>
+          <div v-if="!isDrawing && currentWinners && currentWinners.length > 0"
+            class="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-screen-lg w-full my-4">
+            <OpaqueBox class="drawing-result p-6 rounded-lg space-y-4">
+              <p class="font-mono font-semibold text-2xl text-center">Người Thắng Giải</p>
+              <div class="flex flex-col items-center space-y-2">
+                <div v-for="winner in currentWinners" :key="winner.id"
+                  class="flex justify-center items-center space-x-4 text-xl font-semibold"
+                  :class="winner.type === 'factory' ? 'text-blue-400' : 'text-slate-800'">
+                  <p class="font-sans">{{ winner.code }}</p>
+                  <span>-</span>
+                  <p class="font-sans">{{ winner.name }}</p>
+                </div>
+              </div>
+            </OpaqueBox>
+            <OpaqueBox class="winners-list flex flex-col items-center p-6 rounded-lg space-y-8">
+              <p class="font-mono font-semibold text-2xl text-center">Danh Sách Người Thắng Giải</p>
+              <div class="flex flex-col items-center space-y-4 2xl:h-[52vh] md:h-[36vh] overflow-auto"
+                ref="winner-list">
+                <div v-for="winner in thirdWinners" :key="winner.id"
+                  class="flex justify-center items-center space-x-2 text-xl"
+                  :class="winner.type === 'factory' ? 'text-blue-400' : 'text-slate-800'">
+                  <p class="font-sans font-semibold">{{ winner.code }}</p>
+                  <span>-</span>
+                  <p class="font-sans font-semibold">{{ winner.name }}</p>
+                </div>
+              </div>
+              <div v-if="thirdWinners" class="font-semibold text-xl text-center">
+                <span>Tổng số: </span><span>{{ thirdWinners.length }}</span>
+              </div>
+            </OpaqueBox>
+          </div>
+        </transition>
       </div>
     </div>
+    <Button class="w-32 h-32 rounded-full red-spring text-3xl font-semibold" @click="drawing" :disabled="!drawable">
+      Bốc Giải
+    </Button>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
